b33eaaeefddbc5782014d0d92d7cb2af71586eea
AvishIndu <88189186+AvishIndu@users.noreply.github.com>
2022-02-02 22:32:53 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,17 +1,8 @@
 <?php
-// Get the database name from the environment
-$db = getenv("MYSQL_ADDON_DB");
-// Get the database user from the environment
-$user = getenv("MYSQL_ADDON_USER");
-// Get the database password from the environment
-$pass = getenv("MYSQL_ADDON_PASSWORD");
-// This variable is injected during the deployment
-$socket = getenv("CC_MYSQL_PROXYSQL_SOCKET_PATH");
-$dsn = "mysql:unix_socket=$socket;dbname=$db";
-try {
-     $pdo = new PDO($dsn, $user, $pass);
-} catch (PDOException $e) {
-     throw new PDOException($e->getMessage(), (int)$e->getCode());
-}
+  $con = new PDO(
+    	"mysql:host=" . getenv("bsyyqh1phdgn8rnwir5d-mysql.services.clever-cloud.com") . ";dbname=" . getenv("bsyyqh1phdgn8rnwir5d"),
+    	getenv("uk8xaiuids3mx73a"),
+    	getenv("ZdgnP3twhADdkS95ETiW")
+  );
 
 ?> 
