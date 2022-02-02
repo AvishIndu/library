fea4cdf89585f81b6a01c7bc76c4f194c95d2f92
AvishIndu <88189186+AvishIndu@users.noreply.github.com>
2022-02-02 22:30:12 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,17 @@
 <?php
-	  $bdd = new PDO(
-    	"mysql:host=" . getenv("bsyyqh1phdgn8rnwir5d") . ";dbname=" . getenv("bsyyqh1phdgn8rnwir5d"),
-    	getenv("uk8xaiuids3mx73a"),
-    	getenv("ZdgnP3twhADdkS95ETiW")
-  );
-?>
+// Get the database name from the environment
+$db = getenv("MYSQL_ADDON_DB");
+// Get the database user from the environment
+$user = getenv("MYSQL_ADDON_USER");
+// Get the database password from the environment
+$pass = getenv("MYSQL_ADDON_PASSWORD");
+// This variable is injected during the deployment
+$socket = getenv("CC_MYSQL_PROXYSQL_SOCKET_PATH");
+$dsn = "mysql:unix_socket=$socket;dbname=$db";
+try {
+     $pdo = new PDO($dsn, $user, $pass);
+} catch (PDOException $e) {
+     throw new PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+?> 
